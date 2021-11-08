8f14d6f28cf25f1eb2562499012608b33d31c786
Parksohui <psh990525@naver.com>
2021-11-09 03:22:29 +0900

    feat: login
    login
    : 회원 가입 x -> 회원 가입
    : 회원 가입 o -> 로그인

--- app/src/main/java/com/sample/collathon_practice/LoginActivity.kt
+++ app/src/main/java/com/sample/collathon_practice/LoginActivity.kt
@@ -4,17 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         var SignButton=this.findViewById<Button>(R.id.SignButton)
         SignButton.setOnClickListener{
-            //login 구현 _ 임시로 버튼 누르면 Main으로 넘어가게 해둠
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            var email=this.findViewById<EditText>(R.id.email).getText().toString().trim()
+            var pwd=this.findViewById<EditText>(R.id.pwd).getText().toString().trim()
+
+            if(email=="" || pwd==""){
+                Toast.makeText(applicationContext,"email이나 password를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }else{ //회원 가입 o -> 로그인, 회원 가입 x -> 회원가입
+                FirebaseAuth
+                    .getInstance()
+                    .signInWithEmailAndPassword(email,pwd)
+                    .addOnCompleteListener(this){task->
+                        if(task.isSuccessful){
+                            Toast.makeText(applicationContext,"로그인 성공",Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else{
+                            FirebaseAuth
+                                .getInstance()
+                                .createUserWithEmailAndPassword(email,pwd)
+                                .addOnCompleteListener(this){task ->
+                                    if(task.isSuccessful){
+                                        Toast.makeText(applicationContext,"회원가입 성공",Toast.LENGTH_SHORT).show()
+
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                    else{
+                                        Toast.makeText(applicationContext,"email이나 password를 확인해주세요",Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                        }
+                    }
+            }
         }
     }
 }
