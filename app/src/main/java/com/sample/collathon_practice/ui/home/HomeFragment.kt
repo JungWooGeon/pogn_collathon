064b1b88eb1d154d2dfe3c552eace2d8a6284fb0
Parksohui <psh990525@naver.com>
2021-11-10 03:11:27 +0900

    feat: DB autoid 적용

--- app/src/main/java/com/sample/collathon_practice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sample/collathon_practice/ui/home/HomeFragment.kt
@@ -21,7 +21,6 @@ import kotlinx.android.synthetic.main.item_timecapsule.view.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
     val db = Firebase.firestore
     var user_family=""
@@ -34,8 +33,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
